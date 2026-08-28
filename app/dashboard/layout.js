c8e49badec7e393cc4e0fea339139cbf7ac4d3d7
albertoattie17-apps <albertoattie17@gmail.com>
2026-08-27 23:28:37 -0500

    Update layout.js

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -42,9 +42,9 @@ export default function DashboardLayout({ children }) {
   }
 
   return (
-    <div className="flex">
+    <div className="flex flex-col md:flex-row">
       <Sidebar />
-      <main className="flex-1 p-6 md:p-8">{children}</main>
+      <main className="flex-1 p-4 md:p-8">{children}</main>
     </div>
   );
 }
